7c9fba18750e3748a5cf9426d28228bdc32933a8
nurultr <nurultyasrahma@gmail.com>
2020-12-17 13:55:17 +0700

    latihan_3 memanggil fungsi mod 16 Desember 2020

---
+++ fungsi_mod_menghitung_jumlah_list_baru_20201216.py
@@ -0,0 +1,12 @@
+
+# Memanggil fungsi mod menghitung_jumlah_list_baru_20201216
+
+from menghitung_jumlah_list_baru_20201216 import hitung_jumlah_data,kali_n_dan_list,jumlah_data_baru
+
+daftar_list = [10,30]
+banyak_data = hitung_jumlah_data(daftar_list)
+hasil_kali  = kali_n_dan_list(daftar_list, banyak_data)
+hasil = jumlah_data_baru(hasil_kali)
+print(hasil_kali)
+print(banyak_data)
+print(hasil)
